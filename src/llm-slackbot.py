a8d9eaba1ce1cba315f001c4cd2d78a52fe11b86
John F. Wu <jwuphysics@gmail.com>
2024-05-11 14:53:21 -0400

    sort of fix slackbot, note that actually GPT >> Llama3 8b

--- src/llm-slackbot.py
+++ src/llm-slackbot.py
@@ -1,4 +1,4 @@
-import argparse 
+import argparse
 import chromadb
 import json
 import os
@@ -6,12 +6,21 @@ import re
 import torch
 
 from pathlib import Path
+from pydantic import BaseModel, Field, conlist
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from transformers import AutoTokenizer
 from typing import List
 
-from llama_index.core import Document, PromptTemplate, StorageContext, VectorStoreIndex, get_response_synthesizer
+from llama_index.core import (
+    Document,
+    PromptTemplate,
+    StorageContext,
+    VectorStoreIndex,
+    get_response_synthesizer,
+)
+from llama_index.core import Settings
+from llama_index.core.program import LLMTextCompletionProgram
 from llama_index.core.query_engine import CustomQueryEngine
 from llama_index.core.response_synthesizers import BaseSynthesizer
 from llama_index.core.retrievers import BaseRetriever
@@ -27,20 +36,36 @@ app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Running the LLM RAG-powered Slack bot, AstroArXivBot.')
+    parser = argparse.ArgumentParser(
+        description="Running the LLM RAG-powered Slack bot, AstroArXivBot."
+    )
 
-    parser.add_argument("-l", "--local-llm", type=bool, help="Use LLaMA-3-8B-Instruct (local LLM) instead of OpenAI", default=True)
-    parser.add_argument("-k", "--top-k-papers", type=int, help="Number of top papers to retrieve", default=3)
+    parser.add_argument(
+        "--local-llm",
+        action=argparse.BooleanOptionalAction,
+        help="Use LLaMA-3-8B-Instruct (local LLM) instead of OpenAI",
+    )
+    parser.add_argument(
+        "-k",
+        "--top-k-papers",
+        type=int,
+        help="Number of top papers to retrieve",
+        default=3,
+    )
 
     args = parser.parse_args()
     return args
 
+
 def initialize_models(local_llm=True):
 
     embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    Settings.embed_model = embed_model
 
     if local_llm:
         model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+        print(f"Using LOCAL MODEL: {model_name}")
+
         tokenizer = AutoTokenizer.from_pretrained(model_name, token=hf_token)
 
         stopping_ids = [
@@ -55,105 +80,188 @@ def initialize_models(local_llm=True):
                 "torch_dtype": torch.bfloat16,
             },
             generate_kwargs={
-                "do_sample": True,
-                "temperature": 0.1,
-                "top_p": 0.95
+                "do_sample": False,
+                "pad_token_id": tokenizer.eos_token_id,
             },
             tokenizer_name="meta-llama/Meta-Llama-3-8B-Instruct",
             tokenizer_kwargs={"token": hf_token},
             stopping_ids=stopping_ids,
         )
+        Settings.llm = llm
+        Settings.tokenizer = tokenizer
     else:
-        llm = OpenAI(model="gpt-4-vision-preview")
-
-    return embed_model, llm
+        gpt_model_name = "gpt-4-vision-preview"
+        llm = OpenAI(model=gpt_model_name, temperature=0.0)
+        print(f"Using GPT model: {gpt_model_name}")
+        Settings.llm = llm
 
 
 qa_prompt = PromptTemplate(
-    "You are an astronomy research assistant that can access arXiv/astro-ph papers as context "
+    "You are an astronomy research assistant that can access arXiv paper chunks as context "
     "to answer user queries."
-    "Given the arXiv papers, answer the query like a professional research astronomer. "
-    "ALWAYS cite referenced papers if they support your answer; "
-    "otherwise don't reference the arXiv papers or references within them. "
-    "Prioritize more recent results. Answer in 100 words or fewer. " # really should say 100 words for GPT-4, 50 words for Llama-3 because it is so verbose
-    "If you don't know the answer based on the papers then say 'I cannot answer.'\n" 
-    "arXiv astro-ph papers are below:\n"
+    "Given the context, answer the query like a professional research astronomer. "
+    "Match the level of specificity or generality as the query. "
+    "ALWAYS cite ALL relevant papers using EXACTLY the citation style in the context, "
+    "in parentheses: `(<https://arxiv.org/abs/1406.2364|1406.2364>)`."
+    "The current year is 2024. Unless directed others, prioritize MORE RECENT results based on the paper YEAR. "
+    "Answer in 100 words or fewer. "  # maybe more like 50 for Llama-3...
+    "If the query is not related to astronomy in any way, or if none of the papers can help "
+    "you answer this, then say 'I cannot answer'. (But only do this sparingly.)\n\n"
+    "The arXiv astro-ph papers context string are below:\n"
     "---------------------\n"
     "{context_str}\n"
-    "---------------------\n"
+    "---------------------\n\n"
     "Query: {query_str}\n"
     "Answer: "
 )
+
 
 def create_query_handler(query_engine):
     """Factory method that handles the Slack bot while loading the appropriate
     query_engine instance and returns the event handler
     """
-    
+
     def query_event_handler(event, say):
         try:
-            channel_id = event['channel']
-            thread_ts = event['ts']
-            full_user_query = event['text']
+            channel_id = event["channel"]
+            thread_ts = event["ts"]
+            full_user_query = event["text"]
 
             # strip out the bot name mention from the query
             user_query = re.sub(r"<@[^>]+>", "", full_user_query).strip()
 
             # user_query = body["event"]["blocks"][0]["elements"][0]["elements"][1]["text"]
             if user_query:
+
+                # for logging purposes
                 print(f"Query received: {user_query}\n")
+
                 response = query_engine.custom_query(user_query)
                 print(f"Response: {response}\n")
 
-                # this part goes to Slack
-                say(response, channel=channel_id, thread_ts=thread_ts)
+                # this part goes to Slack!
+                say(
+                    response,
+                    channel=channel_id,
+                    thread_ts=thread_ts,
+                    unfurl_links=False,
+                )
         except Exception as e:
-            print("Error: %s"%e)
+            print("Error: %s" % e)
 
     return query_event_handler
 
+
 def process_json_data(directory_path: str | os.PathLike) -> List[Document]:
-    """Helper function to convert arXiv data into a list of LlamaIndex Documents
-    """
+    """Helper function to convert arXiv data into a list of LlamaIndex Documents"""
     processed_docs = []
     for root, dirs, files in os.walk(directory_path):
         for filename in files:
             if filename.endswith(".json"):
                 file_path = os.path.join(root, filename)
-                with open(file_path, 'r') as file:
+                with open(file_path, "r") as file:
                     doc = json.load(file)
                     for document_key in doc:
-                        if "abstract" in doc[document_key] and (doc[document_key]["abstract"] != ""): # ensure all nodes have some valid string..
+                        if "abstract" in doc[document_key] and (
+                            doc[document_key]["abstract"] != ""
+                        ):  # ensure all nodes have some valid string..
                             document = Document(
                                 doc_id=document_key,
-                                text=doc[document_key]["abstract"], 
-                                metadata={'doc_id': document_key, 'filepath': file_path, 'yymm': filename}
+                                text=doc[document_key]["abstract"],
+                                metadata={
+                                    "doc_id": document_key,
+                                    "filepath": file_path,
+                                    "yymm": filename,
+                                },
                             )
                             processed_docs.append(document)
     return processed_docs
 
-def format_node_data(node: TextNode) -> str:
-    """Convert LlamaIndex Document (TextNode) objects into RAG context string.
+
+class ArXivPaper(BaseModel):
+    """Data model for arXiv astro-ph paper."""
+
+    year: int
+    arxiv_id: str = Field(..., description="arXiv identifier")
+    summary: str = Field(..., description="one sentence summary of the paper")
+
+
+def model_validate_arxiv_paper(formatted_context: str, max_retry=5) -> ArXivPaper:
+    """A model validation program that prompts (and forces) some formatted_context
+    (i.e. output of `format_node_data()`) to adhere to the ArXivPaper model.
+
+    Honestly the JSON completion for LLaMA-3 is somewhat unreliable so it may be better to
+    either patch in openai here, or simply skip this until we have a better option.
     """
-    
+    validated_output = LLMTextCompletionProgram.from_defaults(
+        output_cls=ArXivPaper,
+        llm=Settings.llm,
+        prompt_template_str=(
+            "Please extract the following text into a structured ArXivPaper object: {formatted_context}. "
+            "Make sure it is valid python dict that does not include extra characters like newlines. "
+            "Provide a concise summary (one sentence). "
+            "Extract the arXiv id like '1501.00001' or 'astro-ph9905116'; "
+            "it should NOT contain the word 'arXiv' in it. "
+        ),
+    )
+
+    retries = 0
+    while retries < max_retry:
+        try:
+            return validated_output(formatted_context=formatted_context)
+        except Exception as e:
+            retries += 1
+            print(f"Retry #{retries}", e)
+    return None
+
+
+def make_arxiv_link(arxiv_id):
+    return f"https://arxiv.org/abs/{arxiv_id if '.' in arxiv_id else arxiv_id.replace('astro-ph', 'astro-ph/')}"
+
+
+def format_node_data(node: TextNode, improved_validation=True) -> str:
+    """Convert LlamaIndex Document (TextNode) objects into RAG context string.
+    Uses the model_validate_arxiv_paper() structured extraction if `improved_validation`
+    is set to True.
+    """
+
     doc_id = node.metadata["doc_id"]
     filepath = node.metadata["filepath"]
     yymm = node.metadata["yymm"]
-    
-    abs = node.text
-    
-    with open(filepath, 'r') as file:
+
+    abstract = node.text
+
+    with open(filepath, "r") as file:
         doc = json.load(file)
-        conc = doc[doc_id]["conclusions"]
+        conclusions = doc[doc_id]["conclusions"]
+
+    arxiv_id = doc_id.replace("_arXiv", "")
 
     formatted_context = (
-        f"arXiv ID: {doc_id}\n\n"
-        f"(Year: {'19' + yymm[:2] if int(yymm[:4]) > 9000 else '20'+yymm[:2]})\n\n"
-        f"ABSTRACT: {abs}\n\n"
-        f"CONCLUSIONS: {conc}"
+        f"CITATION: '<{make_arxiv_link(arxiv_id)}|{arxiv_id}>'\n\n"
+        f"YEAR: {'19' + yymm[:2] if int(yymm[:4]) > 9000 else '20' + yymm[:2]}\n\n"
+        f"ABSTRACT: {abstract}\n\n"
+        # f"CONCLUSIONS: {conclusions}\n"
     )
 
-    return formatted_context
+    # DON'T USE FOR NOW -- FAILS TOO OFTEN WITH LOCAL MODELS
+    if improved_validation:
+        output = model_validate_arxiv_paper(formatted_context)
+
+        if output is None:
+            return formatted_context
+
+        improved_formatted_context = (
+            f"CITATION: '<{make_arxiv_link(output.arxiv_id)}|{output.arxiv_id}>'\n\n"
+            f"YEAR: {output.year}\n\n"
+            f"SUMMARY: {output.summary}\n\n"
+            f"ABSTRACT: {abstract}\n\n"
+            f"CONCLUSIONS: {conclusions}\n"
+        )
+        return improved_formatted_context
+    else:
+        return formatted_context
+
 
 class ArxivRetrievalQueryEngine(CustomQueryEngine):
     """arXiv/astroph RAG query engine."""
@@ -166,16 +274,24 @@ class ArxivRetrievalQueryEngine(CustomQueryEngine):
     def custom_query(self, query_str: str):
         nodes = self.retriever.retrieve(query_str)
 
-        context_str = "\n\n".join([format_node_data(n) for n in nodes])
-        response = self.llm.complete(
+        # converts documents -> formatted strings
+        formatted_contexts = [
+            format_node_data(n, improved_validation=False) for n in nodes
+        ]
+
+        context_str = "\n\n".join(formatted_contexts)
+        response = Settings.llm.complete(
             qa_prompt.format(context_str=context_str, query_str=query_str)
         )
 
         return str(response)
 
-def build_query_engine(index, llm, top_k_papers=5, response_mode="refine") -> ArxivRetrievalQueryEngine:
-    """convenience function that returns the query engine defined in the 
-    above class. 
+
+def build_query_engine(
+    index, top_k_papers=5, response_mode="refine"
+) -> ArxivRetrievalQueryEngine:
+    """convenience function that returns the query engine defined in the
+    above class.
 
     Params
     top_k_papers: int
@@ -183,24 +299,27 @@ def build_query_engine(index, llm, top_k_papers=5, response_mode="refine") -> Ar
     response_mode: str -- must be "refine" or "compact"
         how should the LLM integrate the context?
 
-    `llm` and `qa_prompt` are global vars
+    `qa_prompt` is a global vars
     """
     retriever = index.as_retriever(similarity_top_k=top_k_papers)
-    synthesizer = get_response_synthesizer(llm=llm, text_qa_template=qa_prompt, response_mode=response_mode)
-    
+    synthesizer = get_response_synthesizer(
+        llm=Settings.llm, text_qa_template=qa_prompt, response_mode=response_mode
+    )
+
     query_engine = ArxivRetrievalQueryEngine(
-        retriever=retriever, 
-        response_synthesizer=synthesizer, 
-        llm=llm, 
-        qa_prompt=qa_prompt
+        retriever=retriever,
+        response_synthesizer=synthesizer,
+        llm=Settings.llm,
+        qa_prompt=qa_prompt,
     )
 
     return query_engine
 
+
 if __name__ == "__main__":
 
     args = parse_arguments()
-    embed_model, llm = initialize_models(local_llm=args.local_llm)
+    initialize_models(local_llm=args.local_llm)
 
     print(f"Working in {ROOT} directory")
     db = chromadb.PersistentClient(path=f"{ROOT}/chroma_db")
@@ -215,26 +334,30 @@ if __name__ == "__main__":
         arxiv_docs = process_json_data(docs_path)
 
         print(f"Building vector store for {len(arxiv_docs)} documents...")
-        
-        # for building vector store 
+
+        # for building vector store
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
         index = VectorStoreIndex.from_documents(
-            arxiv_docs, storage_context=storage_context, embed_model=embed_model, 
+            arxiv_docs,
+            storage_context=storage_context,
+            embed_model=Settings.embed_model,
         )
     # otherwise, build index
     else:
         print(f"Loading {n_docs} documents...")
         index = VectorStoreIndex.from_vector_store(
-            vector_store, embed_model=embed_model
+            vector_store, embed_model=Settings.embed_model
         )
 
-    # build query engine (using global vars for llm and qa_prompt... see top!)
-    query_engine = build_query_engine(index, llm=llm, top_k_papers=args.top_k_papers, response_mode="refine")
-    
+    # build query engine (using global var for qa_prompt... see top!)
+    query_engine = build_query_engine(
+        index, top_k_papers=args.top_k_papers, response_mode="compact"
+    )
+
     # initialize the Slack listener function for @mentions and direct messages
     handler = create_query_handler(query_engine)
-    app.event("app_mention")(handler) 
-    app.event("message")(handler) 
+    app.event("app_mention")(handler)
+    app.event("message")(handler)
 
     # start Slack app
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
